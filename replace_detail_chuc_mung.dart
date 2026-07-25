37daadadc8e306728a89293f50d02903d44c3f4f
namtv95it <111723138+namtv95it@users.noreply.github.com>
2026-07-25 23:07:01 +0700

    .

--- replace_detail_chuc_mung.dart
+++
@@ -1,9 +0,0 @@
-import 'dart:io';
-
-void main() {
-  final file = File('lib/screens/detail_screen.dart');
-  var content = file.readAsStringSync();
-  content = content.replaceAll("'🎉'", "t('congratulation_word')");
-  content = content.replaceAll("'Chúc mừng'", "t('congratulation_word')");
-  file.writeAsStringSync(content);
-}
--- test.dart
+++
@@ -1,1 +0,0 @@
-void main() { final t = DateTime(2027,1,1).difference(DateTime(2026,7,21)).inDays; print(t); }
